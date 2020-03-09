b5a3546868ec457c26ccd5f1dca9c4d5795a5bb7
charlie brannan <charliebrannan154@gmail.com>
2020-03-09 17:34:53 +1100

    modliquid name system

--- ModLiquid.cs
+++ ModLiquid.cs
@@ -1,10 +1,28 @@
-﻿using Terraria;
+﻿using System.Text.RegularExpressions;
+using Terraria;
 
 namespace TerraScience
 {
 	public class ModLiquid
 	{
-		//TODO: Hook when a player is in any liquid
+		public virtual string DisplayName { get; set; } = string.Empty;
+
+		public ModLiquid() {
+			DisplayName = ToString();
+		}
+
 		public virtual void InLiquid(Player player) { }
+
+		public virtual void OnLiquidCollide() { }
+
+		public override string ToString() {
+			return Regex.Replace(GetType().ToString(), "([A-Z])", " $1", RegexOptions.Compiled).Trim();
+		}
+	}
+
+	public class TestLiquid : ModLiquid {
+		public override void OnLiquidCollide() {
+			base.OnLiquidCollide();
+		}
 	}
 }
