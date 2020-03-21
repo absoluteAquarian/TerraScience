9421aa864455d597aeff89526150ffb281b733c7
charlie brannan <charliebrannan154@gmail.com>
2020-03-21 12:31:46 +1100

    testest

--- Content/Tiles/Multitiles/SaltExtractor.cs
+++ Content/Tiles/Multitiles/SaltExtractor.cs
@@ -98,8 +98,11 @@ namespace TerraScience.Content.Tiles.Multitiles{
 				float curWaterRatio = se.StoredWater / SaltExtractorEntity.MaxWater;
 				float invRatio = 1f - curWaterRatio;
 				Vector2 offset = new Vector2(2, 12 + maxWaterDrawDiff * invRatio);
-				//This added offset is needed to draw the bars at the right positions.  Not sure why
-				//offset += new Vector2(12) * 16;
+
+				//This added offset is needed to draw the bars at the right positions no matter the lighting mode
+				if(Lighting.lightMode !< 2)
+					offset += new Vector2(12) * 16;
+
 				Point drawPos = (se.Position.ToVector2() * 16 - Main.screenPosition + offset).ToPoint();
 
 				//Draw the first water bar
---
+++ MyLiquidLoader.cs
@@ -0,0 +1,12 @@
+﻿using TerraScience.API.Classes.ModLiquid;
+using TerraScience.Systems.TemperatureSystem;
+
+namespace TerraScience {
+	public class MyLiquidLoader : ModLiquidLoader {
+		public ModLiquid mercuryLiquid = null;
+
+		public override void LoadLiquids(ModLiquidFactory liquidFactory) {
+			mercuryLiquid = liquidFactory.Create("Mercury", "Mercury", "Content/Liquids/Mercury", new DefaultTemperature(8, 5, 4, 6));
+		}
+	}
+}
