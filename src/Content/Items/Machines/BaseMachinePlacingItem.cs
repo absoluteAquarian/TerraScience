7537cae6791d6b2f0dbc122eec651a45ba04fff7
absoluteAquarian <30454621+absoluteAquarian@users.noreply.github.com>
2023-01-06 11:19:47 -0600

    library update + added missing sprites

---
+++ src/Content/Items/Machines/BaseMachinePlacingItem.cs
@@ -0,0 +1,17 @@
+﻿using SerousEnergyLib.Items;
+using SerousEnergyLib.Tiles;
+using Terraria.ModLoader;
+
+namespace TerraScience.Content.Items.Machines {
+	public abstract class BaseMachinePlacingItem : BaseMachineItem {
+		public override string Texture => base.Texture.Replace("Content", "Assets");
+	}
+
+	public abstract class BaseMachinePlacingItem<T> : BaseMachineItem<T> where T : ModTile, IMachineTile {
+		public override string Texture => base.Texture.Replace("Content", "Assets");
+	}
+
+	public abstract class BaseDatalessMachinePlacingItem<TItem, TTile> : DatalessMachineItem<TItem, TTile> where TItem : BaseMachineItem<TTile> where TTile : ModTile, IMachineTile {
+		public override string Texture => base.Texture.Replace("Content", "Assets");
+	}
+}
